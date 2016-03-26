10146e7cfa9a7bf3886d0f2ec169b4a3be1204c6
gperniola <g.perniola22@gmail.com>
2016-03-26 12:00:53 +0100

    added script file for running tests

---
+++ Shortify-server/test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# this script executes the jar containing the JUnit tests for shortify-server
+
+echo "executing JUnit tests..."
+/usr/lib/jvm/java-8-openjdk-amd64/bin/java -jar target/shortify-server-test-jar-with-dependencies.jar
